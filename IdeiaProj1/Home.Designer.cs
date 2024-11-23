4f37cfd0d85e9d775924be52f421fbef5960c86b
José Eduardo <163062463+JoseSouza2007@users.noreply.github.com>
2024-11-23 12:13:52 -0300

    erei o tamanho do trem

--- IdeiaProj1/Home.Designer.cs
+++ IdeiaProj1/Home.Designer.cs
@@ -42,7 +42,7 @@
             this.panel1.Controls.Add(this.label2);
             this.panel1.Location = new System.Drawing.Point(1, 1);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1530, 81);
+            this.panel1.Size = new System.Drawing.Size(1524, 81);
             this.panel1.TabIndex = 25;
             // 
             // label2
